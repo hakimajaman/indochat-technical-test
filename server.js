5ec76f40e47214415d8ec4cae478eac410fc367b
Rifqi Taufiqul Hakim <rifqitaufiqulhakim199922@gmail.com>
2021-09-09 00:54:45 +0700

    add server from express

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const path = require("path");
 const cors = require("cors");
 
@@ -11,5 +11,5 @@ app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "dist", "index.html"));
 });
 app.listen(PORT, () =>
-  console.log("App is running now, yeeee! Alhamdulillah!")
+  console.log("App is running now, yeeee! Alhamdulillah!, port=" + PORT)
 );
